e46b26ff012123d6e6a91db0a18e5e91d2bc1e16
nikoladanilovic <nikola.danilovic28@gmail.com>
2025-06-06 12:32:10 +0200

    Postavljena osnovna CRUD funkcionalnost s jako malo validacija unosa

--- BootcampApp/WebAPI/Controllers/RestaurantRelatedValuesController.cs
+++ BootcampApp/WebAPI/Controllers/RestaurantRelatedValuesController.cs
@@ -29,34 +29,71 @@ namespace WebAPI.Controllers
             "Spagetti", "Pork", "Lamb", "Crabs", "Shrimps"
         };
 
-        [HttpGet("get-menu")]
-        public IEnumerable<RestaurantOrder> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new RestaurantOrder
-            {
-                Id = Guid.NewGuid().ToString(),
-                DishName = DishNames[Random.Shared.Next(DishNames.Length)],
-                PriceOfDish = Random.Shared.Next(-20, 55)
-            })
-            .ToArray();
-        }
+        //[HttpGet("get-menu")]
+        //public IEnumerable<RestaurantOrder> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new RestaurantOrder
+        //    {
+        //        Id = Guid.NewGuid().ToString(),
+        //        DishName = DishNames[Random.Shared.Next(DishNames.Length)],
+        //        PriceOfDish = Random.Shared.Next(-20, 55)
+        //    })
+        //    .ToArray();
+        //}
 
         // POST api/<RestaurantRelatedValuesController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult CreateDish([FromBody] RestaurantOrder dish)
         {
+            dish.Id = listOfAvailableDishes.Count + 1;
+            listOfAvailableDishes.Add(dish);
+            return CreatedAtAction(nameof(GetDishById), new { id = dish.Id }, dish);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetDishById(int id)
+        {
+            var dish = listOfAvailableDishes.FirstOrDefault(p => p.Id == id);
+            if (dish == null)
+                return NotFound();
+
+            return Ok(dish);
         }
 
         // PUT api/<RestaurantRelatedValuesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult ChangeDish(int id, [FromBody] RestaurantOrder dish)
         {
+            var dishToBeChanged = listOfAvailableDishes.FirstOrDefault(p => p.Id == id);
+            dishToBeChanged.DishName = dish.DishName;
+            dishToBeChanged.PriceOfDish = dish.PriceOfDish;
+            if (dishToBeChanged == null)
+                return NotFound();
+
+            return Ok(dishToBeChanged);
         }
 
         // DELETE api/<RestaurantRelatedValuesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public string Delete(int id)
         {
+            bool hasDishWithId = listOfAvailableDishes.Any(p => p.Id == id);
+            if (hasDishWithId)
+            {
+                listOfAvailableDishes.RemoveAll(p => p.Id == id);
+                return "Dish with an id: " + id + " has been removed!";
+            }
+            else
+            {
+                return "There is no dish with the id value " + id;
+            }
+            
         }
+
     }
 }
--- BootcampApp/WebAPI/RestaurantOrder.cs
+++ BootcampApp/WebAPI/RestaurantOrder.cs
@@ -2,7 +2,7 @@
 {
     public class RestaurantOrder
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string DishName { get; set; }
         public double PriceOfDish { get; set; }
 
