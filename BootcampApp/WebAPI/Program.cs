1af1dcedbfeafbe2b968d18f3a9b4be5912213b2
nikoladanilovic <nikola.danilovic28@gmail.com>
2025-06-12 11:10:44 +0200

    Autofac DI implemented

--- BootcampApp/WebAPI/Program.cs
+++ BootcampApp/WebAPI/Program.cs
@@ -1,19 +1,43 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using BootcampApp.Repository;
+using BootcampApp.Service;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Replace default ServiceProvider with Autofac
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
 // Register the services for dependency injection
-builder.Services.AddScoped<IMenuCategoryRepository, MenuCategoryRepository>();
-builder.Services.AddScoped<IMenuItemRepository, MenuItemRepository>();
 
-builder.Services.AddScoped<BootcampApp.Service.IMenuCategoryService, BootcampApp.Service.MenuCategoryService>();
-builder.Services.AddScoped<BootcampApp.Service.IMenuItemService, BootcampApp.Service.MenuItemService>();
+//builder.Services.AddScoped<IMenuCategoryRepository, MenuCategoryRepository>();
+//builder.Services.AddScoped<IMenuItemRepository, MenuItemRepository>();
+
+//builder.Services.AddScoped<BootcampApp.Service.IMenuCategoryService, BootcampApp.Service.MenuCategoryService>();
+//builder.Services.AddScoped<BootcampApp.Service.IMenuItemService, BootcampApp.Service.MenuItemService>();
 
 
+// 3. Configure Autofac-specific registrations
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    containerBuilder.RegisterType<MenuCategoryRepository>()
+                    .As<IMenuCategoryRepository>()
+                    .InstancePerLifetimeScope(); // Equivalent to Scoped
+    containerBuilder.RegisterType<MenuItemRepository>()
+                .As<IMenuItemRepository>()
+                .InstancePerLifetimeScope(); // Equivalent to Scoped
+    containerBuilder.RegisterType<MenuCategoryService>()
+                .As<IMenuCategoryService>()
+                .InstancePerLifetimeScope(); // Equivalent to Scoped
+    containerBuilder.RegisterType<MenuItemService>()
+                .As<IMenuItemService>()
+                .InstancePerLifetimeScope(); // Equivalent to Scoped
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
